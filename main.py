3deb16139290774035872d0b9be885347b7fa1ae
Hamza Gabajiwala <hamzajg16@gmail.com>
2023-08-18 17:46:18 +0530

    cleaning up

--- main.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from keras.datasets import mnist, fashion_mnist, cifar10
-from gand import data, visualise, models, MLConfig, architecture
-
-visualise.plot_classes(mnist,
-                       savefig=True,
-                       fontsize=30, figname='mnist')
